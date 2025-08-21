5a62278a02c885a47a109e4eb9dad579d2068305
JuanMV14 <juanjomontoya0514@gmail.com>
2025-08-21 15:40:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import plotly.graph_objs as go
 # CONFIGURACIÓN SUPABASE
 # -------------------
 SUPABASE_URL = "https://ejsakzzbgwymptqjoigs.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."  # Reemplaza con tu clave real
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqc2FrenpiZ3d5bXB0cWpvaWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzOTQwOTMsImV4cCI6MjA3MDk3MDA5M30.IwadYpEJyQAR0zT4Qm6Ae1Q4ac3gqRkGVz0xzhRe3m0"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.set_page_config(page_title="💰 Finanzas Personales", layout="wide")
